8f530d53b9d728387ebccb9b96ac044ab7948a69
krishna <krishna.sannidhi@gmail.com>
2023-08-28 06:37:43 -0500

    added zustand package and populated board data

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,14 +1,23 @@
+"use client";
+import { useBoardStore } from "@/store/BoardStore";
+import { useEffect } from "react";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 function Board() {
+  const [board, getBoard] = useBoardStore((state) => [
+    state.board,
+    state.getBoard,
+  ]);
+
+  console.log("board", board);
+  useEffect(() => {
+    getBoard();
+  }, [getBoard]);
+
   return (
     <DragDropContext>
-      <Droppable
-        droppableId="board"
-        direction="horizontal"
-        type="column"
-      >
-        {provided => (<div></div>)}
+      <Droppable droppableId="board" direction="horizontal" type="column">
+        {(provided) => <div></div>}
       </Droppable>
     </DragDropContext>
   );
