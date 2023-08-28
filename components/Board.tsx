34cfa534148580b7135a2a6e4aeef912b6f83c0d
krishna <krishna.sannidhi@gmail.com>
2023-08-28 07:21:25 -0500

    draggable column and card wip

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,7 +1,8 @@
 "use client";
 import { useBoardStore } from "@/store/BoardStore";
 import { useEffect } from "react";
-import { DragDropContext, Droppable } from "react-beautiful-dnd";
+import { DragDropContext, DropResult, Droppable } from "react-beautiful-dnd";
+import Column from "./Column";
 
 function Board() {
   const [board, getBoard] = useBoardStore((state) => [
@@ -14,10 +15,22 @@ function Board() {
     getBoard();
   }, [getBoard]);
 
+  const handleOnDragEnd = (result: DropResult) => {};
+
   return (
-    <DragDropContext>
+    <DragDropContext onDragEnd={handleOnDragEnd}>
       <Droppable droppableId="board" direction="horizontal" type="column">
-        {(provided) => <div></div>}
+        {(provided) => (
+          <div
+            className="grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto"
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+          >
+            {Array.from(board.columns.entries()).map(([id, column], index) => (
+              <Column key={id} id={id} todos={column.todos} index={index} />
+            ))}
+          </div>
+        )}
       </Droppable>
     </DragDropContext>
   );
---
+++ components/TodoCard.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { XCircleIcon } from "@heroicons/react/24/solid";
+import {
+  DraggableProvidedDragHandleProps,
+  DraggableProvidedDraggableProps,
+} from "react-beautiful-dnd";
+
+type TodoCardProps = {
+  todo: Todo;
+  index: number;
+  id: TypedColumn;
+  innerRef: (element: HTMLElement | null) => void;
+  draggableProps: DraggableProvidedDraggableProps;
+  dragHandleProps: DraggableProvidedDragHandleProps | null | undefined;
+};
+
+function TodoCard({
+  todo,
+  index,
+  id,
+  innerRef,
+  dragHandleProps,
+  draggableProps,
+}: TodoCardProps) {
+  return (
+    <div
+      {...dragHandleProps}
+      {...draggableProps}
+      className="bg-white rounded-md space-y-2 drop-shadow-md"
+      ref={innerRef}
+    >
+      <div className="flex justify-between items-center p-5">
+        <p>{todo.title}</p>
+        <button className="text-red-500 hover:text-red-600">
+          <XCircleIcon className="ml-5 h-8 w-8" />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default TodoCard;
