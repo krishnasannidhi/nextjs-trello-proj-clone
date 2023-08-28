ec2d49c34b6a6493ca52a7b07db960de71dda600
krishna <krishna.sannidhi@gmail.com>
2023-08-27 21:43:29 -0500

    Header component with page background

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import Board from "@/components/Board";
 import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <main>
       <Header />
-      <h1>Trello 2.0 AI Clone</h1>
+      <Board />
     </main>
   );
 }
